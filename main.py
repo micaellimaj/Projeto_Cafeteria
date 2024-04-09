e1b064d5488bb3cd1bf0cce07a77d0db734183b4
WendelAgra <wendelvinicius123@hotmail.com>
2024-04-09 00:52:34 -0300

    parte 7 feita, mas é bom tu olhar, pq não rodou comigo
    parte 7
    
    Co-Authored-By: Micael Lima  <131397330+micaellimaj@users.noreply.github.com>

--- main.py
+++ main.py
@@ -55,6 +55,48 @@ class AppWindow:
         porcentagem_1 = Label(self.frameMeio, text="{:,.2f}".format(valor), anchor=NW, font=('Verdana 12'), bg=co1, fg=co4)
         porcentagem_1.place(x=200, y=35)
 
+# funçao para grafico bars
+def grafico_bar():
+    lista_categorias = ['Renda','Despesas','Saldo']
+    lista_valores = [3000, 2000, 6236]
+
+#faça figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(4, 3.45), dpi=60)
+    ax = figura.add_subplot(111)
+   # ax.autoscale(enable=True, axis='both', tight=None)
+
+    ax.bar(lista_categorias, lista_valores,  color=colors, width=0.9)
+    #create a list to collect the plt.patches data
+
+    c = 0
+    #set individual bar lables using above list
+    for i in ax.patches:
+       # get_x pulls left or right; get_height pushes up or down
+        ax.text(i.get_x()-.001, i.get_height()+.5,
+                str("{:,.0f}".format(lista_valores[c])), fontsize=17, fontstyle='italic',  verticalalignment='bottom',color='dimgrey')
+        c += 1
+
+    ax.set_xticklabels(lista_categorias,fontsize=16)
+
+    ax.patch.set_facecolor('#ffffff')
+    ax.spines['bottom'].set_color('#CCCCCC')
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(0)
+    ax.spines['top'].set_linewidth(0)
+    ax.spines['left'].set_color('#CCCCCC')
+    ax.spines['left'].set_linewidth(1)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(False, color='#EEEEEE')
+    ax.xaxis.grid(False)
+
+    canva = FigureCanvasTkAgg(figura, frameMeio)
+    canva.get_tk_widget().place(x=10, y=70)
+grafico_bar()
 def main():
     janela = Tk()
     app = AppWindow(janela)
@@ -62,3 +104,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
