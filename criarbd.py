7b1b3ec91009080256b8c0359d6fb184cff8f10b
Micael Lima <micaeljose144@gmail.com>
2024-05-27 20:04:20 -0300

    inserção form

--- criarbd.py
+++ criarbd.py
@@ -2,20 +2,11 @@
 import sqlite3 as lite
 
 # Criando Conexão
+
+# Criando Conexão
 con = lite.connect('dados.db')
 
-# Criando tabela de categoria
+# Criando tabela de produtos
 with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE Categoria(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)")
-
-# Criando tabela de receitas
-with con:
-    cur = con.cursor()
-    cur.execute("CREATE TABLE Receitas(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, adicionado_em DATE, valor DECIMAL)")
-
-# Criando tabela de gastos
-with con:
-    cur = con.cursor()
-    cur.execute("CREATE TABLE Gastos(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, retirado_em DATE, valor DECIMAL)")
-
+    cur.execute("CREATE TABLE Produtos(codigo INTEGER PRIMARY KEY AUTOINCREMENT, descricao TEXT, categoria TEXT, NCM INTEGER, Und TEXT, estoque INTEGER, custo INTEGER, data DATE)")
