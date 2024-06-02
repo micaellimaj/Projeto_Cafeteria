d9f685eda604e7e9bf028cb876b3a88a18ffa5dc
Micael Lima <micaeljose144@gmail.com>
2024-06-02 17:43:39 -0300

    testes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ colors = ['#5588bb', '#66bbbb','#99bb55', '#ee9944', '#444466', '#bb5555']
 ################# criando janela ###############
 
 root = tk.Tk()
-root.title("Formulário de Inserção de Produto")
+root.title("Formulário de Inserção de Produtos da Confraria do Café")
 root.geometry('900x650')
 root.configure(background=co9)
 root.resizable(width=FALSE, height=FALSE)
@@ -58,14 +58,8 @@ app_logo.place(x=0, y=0)
 def inserir_click():
     nome_produto = entry_produto.get()
     categoria = dropdown_categoria.get()
-    ncm = entry_ncm.get()
     estoque = spinbox_estoque.get()
     custo = spinbox_custo.get()
-    data = entry_data.get()
-    unidade = dropdown_unidade.get()
-    
-    # Convertendo a data para o formato correto
-    data_formatada = datetime.strptime(data, "%d/%m/%Y")
     
     # Estabelecendo conexão com o banco de dados
     conexao = mysql.connector.connect(
@@ -78,10 +72,10 @@ def inserir_click():
     cursor = conexao.cursor()
     
     # Criando a instrução SQL com marcadores de posição
-    comando = 'INSERT INTO produtos (nome_produto, categoria, ncm, estoque, custo, data, und) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+    comando = 'INSERT INTO produtos (nome_produto, categoria, estoque, custo) VALUES (%s, %s, %s, %s)'
     
     # Criando a tupla com os valores
-    valores = (nome_produto, categoria, ncm, estoque, custo, data_formatada, unidade)
+    valores = (nome_produto, categoria, estoque, custo)
     
     # Executando o comando SQL
     cursor.execute(comando, valores)
@@ -101,40 +95,22 @@ entry_produto = tk.Entry(frameInserirProdutos, width=50)
 entry_produto.grid(row=0, column=1)
 
 tk.Label(frameInserirProdutos, text="Categoria:").grid(row=1, column=0)
-categorias = ["Alimento", "Bebida", "Limpeza"]
+categorias = ["alimento", "bebida", "limpeza"]
 dropdown_categoria = tk.StringVar(root)
 dropdown_categoria.set(categorias[0])  # Definindo a categoria padrão
 dropdown_menu = tk.OptionMenu(frameInserirProdutos, dropdown_categoria, *categorias)
 dropdown_menu.config(width=44)
 dropdown_menu.grid(row=1, column=1)
 
-tk.Label(frameInserirProdutos, text="NCM:").grid(row=2, column=0)
-entry_ncm = tk.Entry(frameInserirProdutos, width=50)
-entry_ncm.grid(row=2, column=1)
 
 # Adicionando um label e um Spinbox para inserir o estoque
-tk.Label(frameInserirProdutos, text="Estoque:").grid(row=3, column=0)
+tk.Label(frameInserirProdutos, text="Unidade:").grid(row=3, column=0)
 spinbox_estoque = tk.Spinbox(frameInserirProdutos, from_=1, to=100, width=48)
 spinbox_estoque.grid(row=3, column=1)
-
 
 tk.Label(frameInserirProdutos, text="Custo (R$):").grid(row=4, column=0)
 spinbox_custo = tk.Spinbox(frameInserirProdutos, from_=1.0, to=100.0, increment=0.50, format="%.2f", width=48)
 spinbox_custo.grid(row=4, column=1)
-
-tk.Label(frameInserirProdutos, text="Data: ").grid(row=5, column=0)
-entry_data = tk.Entry(frameInserirProdutos, width=50)
-entry_data.grid(row=5, column=1)
-
-# Adicionando um label e um dropdown para selecionar a unidade
-tk.Label(frameInserirProdutos, text="Unidade:").grid(row=6, column=0)
-unidades = ["FD", "UM", "UN", "LTA", "CX", "MÇ", "FD", "PTE", "CT", "PT", "GFA", "KG"]
-dropdown_unidade = tk.StringVar(root)
-dropdown_unidade.set(unidades[0])  # Definindo a unidade padrão
-dropdown_menu_unidade = tk.OptionMenu(frameInserirProdutos, dropdown_unidade, *unidades)
-dropdown_menu_unidade.config(width=44)
-dropdown_menu_unidade.grid(row=6, column=1)
-
 
 btn_inserir = tk.Button(frameInserirProdutos, width=50, text="Inserir", command=inserir_click)
 btn_inserir.grid(row=7, column=0, columnspan=2, pady=10)
