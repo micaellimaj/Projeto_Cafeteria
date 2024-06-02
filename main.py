db8840daf46d94b0a1c27da07f03e7c27298f173
Micael Lima <micaeljose144@gmail.com>
2024-06-02 19:19:01 -0300

    conf

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ colors = ['#5588bb', '#66bbbb','#99bb55', '#ee9944', '#444466', '#bb5555']
 
 root = tk.Tk()
 root.title("Formulário de Inserção de Produtos da Confraria do Café")
-root.geometry('900x650')
+root.geometry('800x400')
 root.configure(background=co9)
 root.resizable(width=FALSE, height=FALSE)
 
@@ -53,13 +53,24 @@ app_logo = Label(frameCima, image=app_img, text=" GESTÃO DE ESTOQUE", width=900
 
 app_logo.place(x=0, y=0)
 
+
 # ------------------ CONEXÃO COM BANCO DE DADOS -------------------------- #
 
+def limpar_formulario():
+    entry_produto.delete(0, tk.END)  # Limpa o campo de entrada do produto
+    dropdown_categoria.set(categorias[0])  # Define a categoria padrão novamente
+    spinbox_unidade.delete(0, tk.END)  # Limpa o campo de entrada da unidade
+    spinbox_custo.delete(0, tk.END)  # Limpa o campo de entrada do custo
+    entry_data.delete(0, tk.END)  # Limpa o campo de entrada da data
+
+
 def inserir_click():
-    nome_produto = entry_produto.get()
+    nome = entry_produto.get()
     categoria = dropdown_categoria.get()
-    estoque = spinbox_estoque.get()
+    unidade = spinbox_unidade.get()
     custo = spinbox_custo.get()
+    data = entry_data.get()
+    limpar_formulario()
     
     # Estabelecendo conexão com o banco de dados
     conexao = mysql.connector.connect(
@@ -70,12 +81,14 @@ def inserir_click():
     )
     
     cursor = conexao.cursor()
+
+    data_formatada = datetime.strptime(data, "%d/%m/%Y")
     
     # Criando a instrução SQL com marcadores de posição
-    comando = 'INSERT INTO produtos (nome_produto, categoria, estoque, custo) VALUES (%s, %s, %s, %s)'
+    comando = 'INSERT INTO produtos (nome, categoria, unidade, custo, data) VALUES (%s, %s, %s, %s, %s)'
     
     # Criando a tupla com os valores
-    valores = (nome_produto, categoria, estoque, custo)
+    valores = (nome, categoria, unidade, custo, data_formatada)
     
     # Executando o comando SQL
     cursor.execute(comando, valores)
@@ -89,11 +102,12 @@ def inserir_click():
 
 # -------------------------- FORMULÁRIO -------------------------- #
 
-# Criando os widgets
+# Adicionando um label para inserir o nome do produto
 tk.Label(frameInserirProdutos, text="Nome:").grid(row=0, column=0)
 entry_produto = tk.Entry(frameInserirProdutos, width=50)
 entry_produto.grid(row=0, column=1)
 
+# Adicionando um label e um dropdown para inserir a categoria
 tk.Label(frameInserirProdutos, text="Categoria:").grid(row=1, column=0)
 categorias = ["alimento", "bebida", "limpeza"]
 dropdown_categoria = tk.StringVar(root)
@@ -103,19 +117,27 @@ dropdown_menu.config(width=44)
 dropdown_menu.grid(row=1, column=1)
 
 
-# Adicionando um label e um Spinbox para inserir o estoque
+# Adicionando um label e um Spinbox para inserir a unidade
 tk.Label(frameInserirProdutos, text="Unidade:").grid(row=3, column=0)
-spinbox_estoque = tk.Spinbox(frameInserirProdutos, from_=1, to=100, width=48)
-spinbox_estoque.grid(row=3, column=1)
+spinbox_unidade = tk.Spinbox(frameInserirProdutos, from_=1, to=100, width=48)
+spinbox_unidade.grid(row=3, column=1)
 
+# Adicionando um label e um Spinbox para inserir custo
 tk.Label(frameInserirProdutos, text="Custo (R$):").grid(row=4, column=0)
 spinbox_custo = tk.Spinbox(frameInserirProdutos, from_=1.0, to=100.0, increment=0.50, format="%.2f", width=48)
 spinbox_custo.grid(row=4, column=1)
 
+# Adicionando um label -para inserir data
+tk.Label(frameInserirProdutos, text="Data: ").grid(row=5, column=0)
+entry_data = tk.Entry(frameInserirProdutos, width=50)
+entry_data.grid(row=5, column=1)
+
+# Adicionando botão inserir produtos
 btn_inserir = tk.Button(frameInserirProdutos, width=50, text="Inserir", command=inserir_click)
 btn_inserir.grid(row=7, column=0, columnspan=2, pady=10)
 
-
+# Função para limpar o formulário
+limpar_formulario()
 
 # Iniciando o loop principal da aplicação
 root.mainloop()
